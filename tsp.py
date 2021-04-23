b4783543b264a3c3ff922a3118bf5114bfe65cc4
Jack VanDrunen <jackvandrunen@gmail.com>
2021-04-23 13:41:11 -0700

    added method to convert tour segments to cities

--- tsp.py
+++ tsp.py
@@ -92,6 +92,15 @@ class N_TSP:
     def tour_segments(self, tour: Iterable[int]) -> ((int, int)):
         return [self.cities[c] for c in tour] + [self.cities[tour[0]]]
 
+    def convert_tour_segments(self, tour_segments):
+        """Expects a list of coordinates, returns a list of cities"""
+        cities = list(map(tuple, self.cities))
+        tour_segments = list(map(tuple, tour_segments))
+        result = []
+        for coord in tour_segments[:-1]:
+            result.append(cities.index(coord))
+        return result
+
 
 class TSP(N_TSP):
     """Container for a TSP instance"""
