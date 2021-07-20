44e2e0389086b3347a7849dc6b58150af824758a
Jack VanDrunen <jackvandrunen@gmail.com>
2021-07-19 18:37:34 -0700

    docstring and minor linting tweaks for tsp.py

--- tsp.py
+++ tsp.py
@@ -1,4 +1,4 @@
-from typing import Iterable
+from typing import Iterable, Iterator, DefaultDict
 from numpy.typing import ArrayLike, NDArray
 import itertools as it
 import numpy.random as random
@@ -9,7 +9,15 @@ from tsp.templates import Template
 
 
 def _point_to_line(p: ArrayLike, L: ArrayLike) -> float:
-    # see https://stackoverflow.com/a/1501725
+    """Calculates distance from point p to line L. Adapted from https://stackoverflow.com/a/1501725.
+
+    Args:
+        p (ArrayLike): point coordinates [x, y, ...]
+        L (ArrayLike): line coordinates [[x1, y1, ...], [x2, y2, ...]]
+
+    Returns:
+        float: distance
+    """
     p = np.array(p)
     v1, v2 = np.array(L)
     l2 = np.linalg.norm(v1 - v2) ** 2
@@ -21,15 +29,27 @@ def _point_to_line(p: ArrayLike, L: ArrayLike) -> float:
 
 
 def distance(path: Iterable[ArrayLike]) -> float:
-    """Calculate the distance along a path of unspecified length"""
+    """Calculate the distance along a path of unspecified length.
+
+    Args:
+        path (Iterable[ArrayLike]): path as [[x1, y1, ...], ...]
+
+    Returns:
+        float: distance
+    """
     return sum(map(lambda i: np.linalg.norm(path[i - 1] - path[i]), range(1, len(path))))
 
 
 class N_TSP:
-    """Container for an N-dimensional TSP instance"""
+    """Container for a generic TSP instance."""
 
     @classmethod
     def from_cities(cls, cities: ArrayLike):
+        """Generate object from list/array of cities.
+
+        Args:
+            cities (ArrayLike): cities as [[x1, y1, ...], ...]
+        """
         result = cls()
         result.cities = np.array(cities)
         return result
@@ -39,9 +59,19 @@ class N_TSP:
 
     @property
     def dimensions(self) -> int:
+        """Number of dimensions the N_TSP problem is in.
+
+        Returns:
+            int: dimensions
+        """
         return self.cities.shape[1]
 
     def add_city(self, *coords: int):
+        """Inefficiently adds a city to the problem.
+
+        Args:
+            coords ([int]): city coordinates as individual arguments
+        """
         assert self.dimensions == 0 or len(coords) == self.dimensions
         self.dimensions = len(coords)
         self.cities = list(self.cities)
@@ -49,14 +79,32 @@ class N_TSP:
         self.cities = np.array(self.cities)
 
     def edge(self, a: int, b: int) -> float:
+        """Edge length between two cities.
+
+        Args:
+            a (int): index of first city
+            b (int): index of second city
+
+        Returns:
+            float: edge length
+        """
         return np.linalg.norm(self.cities[a] - self.cities[b])
 
-    def to_edges(self) -> Iterable[int, int, float]:
-        """Produces iterable of edges (a, b, d) of distance d between vertices a and b"""
+    def to_edges(self) -> Iterator[int, int, float]:
+        """Produces iterable of edges (a, b, d) of distance d between vertices a and b.
+
+        Yields:
+            Iterator[int, int, float]: edges
+        """
         for a, b in it.combinations(range(len(self.cities)), 2):
             yield a, b, self.edge(a, b)
 
     def to_edge_matrix(self) -> NDArray:
+        """Generate an edge matrix from the problem.
+
+        Returns:
+            NDArray: edge matrix
+        """
         count = len(self.cities)
         result = np.zeros((count, count), dtype=np.float32)
         for a, b, d in self.to_edges():
@@ -65,21 +113,42 @@ class N_TSP:
         result[il] = result.T[il]  # Make symmetric  # pylint: disable=E1136
         return result
 
-    def tour_segments(self, tour: Iterable[int]) -> Iterable[ArrayLike]:
+    def tour_segments(self, tour: Iterable[int]) -> Iterator[ArrayLike]:
+        """Convert a tour in index format into a tour in segment format.
+
+        Args:
+            tour (Iterable[int]): tour as indices of cities
+
+        Yields:
+            Iterator[ArrayLike]: tour as coordinates of cities
+        """
         for c in tour:
             yield self.cities[c]
         yield self.cities[tour[0]]
 
-    def convert_tour_segments(self, tour_segments: Iterable[ArrayLike]) -> NDArray:
-        """Expects a list of coordinates, returns a list of cities"""
+    def convert_tour_segments(self, tour_segments: Iterable[ArrayLike]) -> Iterator[int]:
+        """Convert a tour in segment format into a tour in index format.
+
+        Args:
+            tour_segments (Iterable[ArrayLike]): tour as coordinates of cities
+
+        Yields:
+            Iterator[int]: tour as indices of cities
+        """
         cities = list(map(tuple, self.cities))
         tour_segments = list(map(tuple, tour_segments))
-        result = []
         for coord in tour_segments[:-1]:
-            result.append(cities.index(coord))
-        return np.array(result)
+            yield cities.index(coord)
 
     def score(self, tour: Iterable[int]) -> float:
+        """Calculate tour length (from index format).
+
+        Args:
+            tour (Iterable[int]): tour as indices of cities
+
+        Returns:
+            float: tour length
+        """
         result = 0.
         first = None
         prev = None
@@ -93,37 +162,53 @@ class N_TSP:
         result += self.edge(c, first)
         return result
 
-    def score_tour_segments(self, tour_segments: Iterable[ArrayLike]):
+    def score_tour_segments(self, tour_segments: Iterable[ArrayLike]) -> float:
+        """Calculate tour length (from segment format).
+
+        Args:
+            tour_segments (Iterable[ArrayLike]): tour as coordinates of cities
+
+        Returns:
+            float: tour length
+        """
         return distance(tour_segments)
 
 
 class TSP(N_TSP):
-    """Container for a TSP instance"""
+    """Container for a TSP instance. A wrapper which adds constraints to the N_TSP interface, primarily width and height."""
 
     @classmethod
-    def generate_random(cls, n: int, w: int = 500, h: int = 500, r: int = 10, track_discards: bool = False):
-        j = 0
+    def generate_random(cls, n: int, w: int = 500, h: int = 500, r: int = 10, padding: int = 10):
+        """Generate a new problem with uniformly-distributed random cities.
+
+        Args:
+            n (int): number of cities
+            w (int, optional): Width of problem. Defaults to 500.
+            h (int, optional): Height of problem. Defaults to 500.
+            r (int, optional): Minimum distance between cities. Defaults to 10.
+            padding (int, optional): Minimum distance a city can be from the edge. Defaults to 10.
+        """
         while True:
             result = cls(w, h)
             for _ in range(n):
-                x = random.randint(0, w)
-                y = random.randint(0, h)
+                x = random.randint(padding, w - padding)
+                y = random.randint(padding, h - padding)
                 result.add_city(x, y)
             if min(d for _, __, d in result.to_edges()) >= r:
-                return result if not track_discards else (result, j)
-            j += 1
+                return result
 
     @classmethod
     def from_cities(cls, cities: ArrayLike, w: int = 500, h: int = 500):
+        """Generate object from list/array of cities.
+
+        Args:
+            cities (ArrayLike): cities as [[x1, y1], ...]
+            w (int, optional): Width of problem. Defaults to 500.
+            h (int, optional): Height of problem. Defaults to 500.
+        """
         result = cls(w, h)
         for x, y in cities:
             result.add_city(int(x), int(y))
-        return result
-
-    @classmethod
-    def from_tsp(cls, tsp):
-        result = cls(tsp.w, tsp.h)
-        result.cities = tsp.cities
         return result
 
     def __init__(self, w: int = 500, h: int = 500):
@@ -132,11 +217,23 @@ class TSP(N_TSP):
 
 
 class TSP_O(TSP):
-    """Container for a TSP-with-obstacles instance"""
+    """Container for a TSP-with-obstacles instance."""
 
     @staticmethod
-    def check_min_dist(cities: Iterable[ArrayLike], obstacles: Iterable[ArrayLike], x: int, y: int, r: int, min_dist: int):
-        """Check that (x, y) is not within r of any city, or min_dist of any obstacle"""
+    def _check_min_dist(cities: Iterable[ArrayLike], obstacles: Iterable[ArrayLike], x: int, y: int, r: int, min_dist: int) -> bool:
+        """Check that (x, y) is not within r of any city, or min_dist of any obstacle.
+
+        Args:
+            cities (Iterable[ArrayLike]): cities as [[x1, y1], ...]
+            obstacles (Iterable[ArrayLike]): obstacles as [[[x1, y1], [x2, y2]], ...]
+            x (int): city x-coordinate
+            y (int): city y-coordinate
+            r (int): minimum distance allowed between cities
+            min_dist (int): minimum distance allowed between a city and obstacles
+
+        Returns:
+            bool: whether city can be safely added
+        """
         for p in cities:
             if np.linalg.norm(p - np.array([x, y])) < r:
                 return False
@@ -148,26 +245,35 @@ class TSP_O(TSP):
 
     @classmethod
     def generate_random_safe(cls, n: int, w: int = 500, h: int = 500, r: int = 10, padding: int = 10,
-                             n_obs: int = 10, edge_length: int = 20, min_dist: int = 10, track_discards: bool = False):
-        """Generate a random problem in which cities are min_dist from the obstacles"""
-        j = 0
+                             n_obs: int = 10, edge_length: int = 20, min_dist: int = 10):
+        """Generate a random problem in which cities are min_dist from the obstacles.
+
+        Args:
+            n (int): number of cities
+            w (int, optional): Width of problem. Defaults to 500.
+            h (int, optional): Height of problem. Defaults to 500.
+            r (int, optional): Minimum distance between cities. Defaults to 10.
+            padding (int, optional): Minimum distance a city can be from the edge. Defaults to 10.
+            n_obs (int, optional): Number of obstacles. Defaults to 10.
+            edge_length (int, optional): Length of obstacles. Defaults to 20.
+            min_dist (int, optional): Minimum distance a city can be from an obstacle. Defaults to 10.
+        """
         while True:
             result = cls(w, h)
             result.add_random_obstacles(n_obs, edge_length)
             while len(result.cities) < n:
                 x = random.randint(padding, w - padding)
                 y = random.randint(padding, h - padding)
-                if not TSP_O.check_min_dist(result.cities, result.obstacles, x, y, r, min_dist):
+                if not TSP_O._check_min_dist(result.cities, result.obstacles, x, y, r, min_dist):
                     continue
                 result.add_city(x, y)
             try:
                 result.vg = None
                 result.to_edge_matrix()
             except Exception:
-                j += 1
                 continue
             if len(result.cities) == n:
-                return (result, j) if track_discards else result
+                return result
 
     def __init__(self, w: int = 500, h: int = 500):
         TSP.__init__(self, w, h)
@@ -176,35 +282,72 @@ class TSP_O(TSP):
         self.E = None
 
     def add_obstacle(self, *vertices: int):
-        """Each vertex is an (x, y) two-tuple"""
+        """Inefficiently add obstacles to the problem.
+
+        Args:
+            vertices ([int, int]): obstacle vertices as [x1, y1], ...
+        """
         self.obstacles = list(self.obstacles)
         self.obstacles.append(np.array([tuple(int(i) for i in v) for v in vertices]))
         self.obstacles = np.array(self.obstacles)
 
-    def to_visgraph(self, rebuild: bool = False):
+    def to_visgraph(self, rebuild: bool = False) -> DefaultDict:
+        """Generate and return a visibility graph for the problem.
+
+        Args:
+            rebuild (bool, optional): Whether or not to rebuild from scratch. Defaults to False.
+
+        Returns:
+            DefaultDict: visibility graph
+        """
         if self.vg is None or rebuild:
             self.vg = calculate_visgraph(self.cities, self.obstacles, bound=(self.w, self.h))
         return self.vg
 
     def edge(self, a: int, b: int) -> float:
+        """Calculate shortest path between two cities.
+
+        Args:
+            a (int): index of first city
+            b (int): index of second city
+
+        Returns:
+            float: edge length
+        """
         g = self.to_visgraph()
         A, B = self.cities[a], self.cities[b]
         return distance(shortest_path(A, B, g))
 
     def to_edges(self) -> Iterable[int, int, float]:
-        """Produces iterable of edges (a, b, d) of distance d between vertices a and b"""
+        """Produces iterable of edges (a, b, d) of distance d between vertices a and b.
+
+        Yields:
+            Iterator[int, int, float]: edges
+        """
         g = self.to_visgraph()
         for a, b in it.combinations(range(len(self.cities)), 2):
             A, B = self.cities[a], self.cities[b]
             yield a, b, distance(shortest_path(A, B, g))
 
-    def to_edge_matrix(self, rebuild: bool = False) -> NDArray:
-        if self.E is None or rebuild:
+    def to_edge_matrix(self) -> NDArray:
+        """Generate an edge matrix from the problem.
+
+        Returns:
+            NDArray: edge matrix
+        """
+        if self.E is None:
             self.E = TSP.to_edge_matrix(self)
         return self.E
 
-    def tour_segments(self, tour: Iterable[int]) -> Iterable[ArrayLike]:
-        """Produces iterable of vertices (x, y) that, when connected, make up the tour in obstacle space"""
+    def tour_segments(self, tour: Iterable[int]) -> Iterator[ArrayLike]:
+        """Produces iterator of vertices (x, y) that, when connected, make up the tour in obstacle space.
+
+        Args:
+            tour (Iterable[int]): tour as indices of cities
+
+        Yields:
+            Iterator[ArrayLike]: tour as coordinates of line segments
+        """
         g = self.to_visgraph()
         prev = None
         first = None
@@ -232,6 +375,11 @@ class TSP_O(TSP):
     # Helper methods for making obstacles
 
     def add_random_obstacle(self, edge_length: int = 20):
+        """Add an obstacle with random positioning and rotation.
+
+        Args:
+            edge_length (int, optional): Obstacle length. Defaults to 20.
+        """
         xc, yc = random.randint(0, self.w), random.randint(0, self.h)
         alpha = np.pi * random.rand()
         x1 = xc + int(edge_length * np.cos(alpha) / 2.)
@@ -241,11 +389,24 @@ class TSP_O(TSP):
         self.add_obstacle((x1, y1), (x2, y2))
 
     def add_random_obstacles(self, n: int, edge_length: int = 20):
+        """Add multiple random obstacles.
+
+        Args:
+            n (int): number of obstacles
+            edge_length (int, optional): Obstacle length. Defaults to 20.
+        """
         for _ in range(n):
             self.add_random_obstacle(edge_length)
         self.to_visgraph(True)
 
     def add_random_obstacles_from_template(self, template: Template, n: int = 1, start_offsets = (0, 0)):
+        """Add obstacles using a template.
+
+        Args:
+            template (Template): template
+            n (int, optional): Number of obstacles. Defaults to 1.
+            start_offsets (tuple, optional): Bounds for template (see template documentation). Defaults to (0, 0).
+        """
         for _ in range(n):
             template(
                 (start_offsets[0], self.w - start_offsets[0]),
