02f4203f7704ac9e061838d1c127326318e0b568
Jack VanDrunen <jackvandrunen@gmail.com>
2021-07-30 17:23:32 -0700

    finished documentation for tsp.experiment

--- src/tsp/experiment/run.py
+++ src/tsp/experiment/run.py
@@ -1,12 +1,37 @@
-"""
-Problem set file format:
+"""Script for automating experiments with multiple experimental conditions.
+
+This script can be run on the command line with `python3 -m tsp.experiment.run`.
+The command line documentation is as follows:
+
+```
+usage: run.py [-h] [-s S] id sets
+
+Run a subject on problem sets.
+
+positional arguments:
+  id          Participant identifier
+  sets        Path to list of problem sets
+
+optional arguments:
+  -h, --help  show this help message and exit
+  -s S        Path to save file
+```
+
+### Problem Set File Format
+
+Problem sets (corresponding to experimental conditions) should be saved under a parent directory
+for the data from the experimental condition in a subdirectory named `problems`. The interface will
+save the human-generated tours and times in a subdirectory named `<id>` where &lt;id&gt; is the
+participant identifier. Then, create a "set list file" with the following format:
+
  - List of paths to problem sets (without /problems)
  - Asterisk (*) in front of paths that should be kept in the position that it is in
  - Ampersand (&) in front of paths that should not have their problems presented in random order (after the * if they coincide)
 
 Example:
 
-*./simulations4/data/set_test
+```
+*&./simulations4/data/set_test
 ./simulations4/data/set_16_128
 ./simulations4/data/set_16_192
 ./simulations4/data/set_16_256
@@ -16,7 +41,65 @@ Example:
 ./simulations4/data/set_48_128
 ./simulations4/data/set_48_192
 ./simulations4/data/set_48_256
+```
 
+This file would present the first experimental condition (a test set) first and with problems in
+unrandomized order. Then it would present all of the rest of the experimental conditions in random
+order, with their problems in randomized order.
+
+When provided with a path, the script will generate a "save file" containing the actual order the
+experimental conditions are presented in and whether or not each one is randomized. This is useful
+both as reference and if the subject needs multiple sessions to complete all conditions. Note that
+if an experimental condition has been started on a previous run of the script, it will be skipped
+over on the next run, *even if it hasn't been completed*. In this case, you will need to manually
+remove the offending directory.
+
+### Packaging
+
+With mildly tech-savvy participants, you can package a simple script which calls
+`tsp.experiment.run.run` as a standalone executable using something like
+[PyInstaller](https://www.pyinstaller.org/), and have the participants run themselves on the
+experimental conditions. In the age of COVID-19, we've had to do this ourselves. The basic
+directory structure should look something like:
+
+```
+root
+ |-- run_this.exe
+ |-- problems.txt  # problem set list
+ |-- data
+      |-- condition1
+      |    |-- problems
+      |         |-- 001.tsp
+      |         |-- ...
+      |
+      |-- condition2
+      |    |-- ...
+      |
+      |-- ...
+ |-- batch_ui  # copy of src/tsp/experiment/batch_ui
+      |-- ...
+```
+
+Then the Python script to package into `run_this.exe` would be something like:
+
+```python
+from tsp.experiment.run import run
+
+run(
+    participant='jv',
+    set_list_path='problems.txt',
+    save_file_path='save.txt',
+    ui_root='batch_ui'
+)
+```
+
+Where "jv" gets replaced with the participant identifier. Theoretically this could also be provided
+dynamically if one doesn't want to package individual scripts for each participant. Solutions and
+times will then be saved in `data/condition1/jv`, `data/condition2/jv`, etc.
+
+Note that the files for the web interface need to be provided manually in order for the UI to work
+correctly as a standalone executable. With a native Python install of the library, the UI stuff is
+all handled "under the hood."
 """
 
 from typing import Iterable, Iterator, Tuple
@@ -106,8 +189,7 @@ def run(participant: str, set_list_path: str, save_file_path: str, ui_root: str 
     for path, randomized in problem_sets:
         print('({}) {}'.format(('R' if randomized else ' '), path))
     print()
-    print('Once the participant has completed a set, continue to the next set with ^C')
-    print('Then have the participant refresh the page')
+    print('Have the participant refresh the page after starting each condition here.')
     _confirm()
     for path, randomized in problem_sets:
         if not os.path.exists(os.path.join(path, participant)):
